e661b0197e81a1914cd8486ff36c04b05d2fd985
Sayad Muhammad Shahzaib <smshahzaibshah@gmail.com>
2020-11-26 18:01:08 +0500

    Basic SignIn page Created along with Custom Button

--- Screens/SignIn.js
+++ Screens/SignIn.js
@@ -1,22 +1,62 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { TextInput,StyleSheet, Text, View, Button, Image } from 'react-native';
+import CustomButton from "../component/ButtonComponent";
+
 
 const SignIn=({navigation})=>{
     return(
       <View style={styles.container}>
-        <Text>SignIn Page</Text>
+          <Text style={styles.text}>Sign In</Text>
+        <View style={styles.inputContainer}>
+            <TextInput 
+                style={styles.textInput}
+                placeholder="Enter username/Email"/>
+        </View>
+        <View style={styles.inputContainer}>
+        <TextInput 
+                style={styles.textInput}
+                placeholder="Enter Password"/>
+
+        </View>
+        <View style={styles.button}>
+        <CustomButton text="Sign In" />
+        </View>
+        
       </View>
   );
   }
   
   const styles = StyleSheet.create({
     container:{
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: "center",
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+       //paddingTop: 40,
     },
-
+    textInput:{
+        borderColor: "red",
+        //borderWidth: 2,
+        borderBottomWidth: 2,
+        width: '78%',
+        //borderRadius: 50,
+        fontSize: 16,
+        padding: 10,
+      },inputContainer:{
+        flexDirection: "row",
+        width: '75%',
+        justifyContent: "space-between",
+        alignItems: "flex-end"
+    
+      },
+      text:{
+        fontSize: 32,
+        color: 'darkgrey',
+      },button:{
+          margin: 10,
+          alignItems: "center",
+          justifyContent: "center"
+      }
 
   });
 
