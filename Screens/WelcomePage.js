aea7af0416ed762188386d2cc58fca39061a9633
Sayad Muhammad Shahzaib <smshahzaibshah@gmail.com>
2020-11-26 22:59:07 +0500

    Welcome Page Css Done

--- Screens/WelcomePage.js
+++ Screens/WelcomePage.js
@@ -1,51 +1,53 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
-
+import CustomButton from "../component/ButtonComponent";
 const WelcomeScreen=({navigation})=>{
     return(
       <View style={styles.container}>
-        <View style={styles.textContainer}>
-            <Text style={styles.WelcomeText}>Welcome To WordsWorth</Text>
-        </View>
         <Image style={styles.ImagesSty} source={require('../Images/WelcomePageLogo.png')}/>
         <Text style={styles.SecondText}>Words Worth helps you search books conversion to audiobook, save time and stay organized</Text>
-        <Button title="Get Started" onPress={()=>{}}/>
-        
-        <Text style={styles.thirdText}>By Joining you agree to share information with people in your friendlist</Text>
+        <CustomButton text="Get Started" color="grey" />
 
+        <View style={styles.bottom}>
+          <Text style={styles.thirdText}>By Joining you agree to share information with people in your friendlist</Text>
+        </View>
+        
       </View>
   );
   }
   
   const styles = StyleSheet.create({
-    textContainer:{
-      
-    },
     container:{
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: "center",
-    },
-    WelcomeText:{
-      fontSize: 26,
-      margin: 4,
+      alignItems: 'center'
     },
     ImagesSty:{
       width: 250,
       height: 70,
-      margin: 10,
+      marginTop: 300,
+      justifyContent: "center"
     },SecondText:{
       fontSize: 16,
       paddingTop: 20,
-      width: "90%",
+      width: "75%",
+      justifyContent: "space-between",
+      marginBottom: 30,
     },
     thirdText:{
-      //flex: 1,
-     // justifyContent: 'flex-end',
-     // marginBottom: 36
+    fontSize: 14,
+    justifyContent: "space-between",
+    fontStyle: "italic", 
+    color: "darkgrey"
     }
-
+    ,bottom:{
+      flex: 1,
+      justifyContent: 'flex-end',
+      alignSelf: "center",
+      marginBottom: 10,
+      width: "89%",
+      //justifyContent: "space-between",
+      
+    }
   });
 
   export default WelcomeScreen;
